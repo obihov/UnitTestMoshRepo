a17e12d05f51cd4f050785e74d5965664d578363
Obinna Desktop <javaobinna@gmail.com>
2022-11-20 22:49:29 -0600

    Created a duplicated folder for the Mocking directory as Mock_UseOnly

---
+++ TestNinja.UnitTests/MockTests/MockVideoServiceTests.cs
@@ -0,0 +1,15 @@
+using TestNinja.Mock_UseOnly;
+
+namespace TestNinja.UnitTests.MockTests;
+
+[TestFixture]
+public class MockVideoServiceTests
+{
+
+    public void SetUp()
+    {
+        var x = new EmployeeController();
+    }
+}
+
+ 
---
+++ TestNinja/Mock_UseOnly/InstallerHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+namespace TestNinja.Mock_UseOnly
+{
+    public class InstallerHelper
+    {
+        private string _setupDestinationFile;
+
+        public bool DownloadInstaller(string customerName, string installerName)
+        {
+            this._setupDestinationFile = String.Empty;
+            
+            var client = new WebClient();
+            try
+            {
+                var url = $"https://example.com/{customerName}/{installerName}"; //string.Format(@"http://example.com/{0}/{1}", customerName, installerName);
+                client.DownloadFile(url, _setupDestinationFile);
+                return true;
+            }
+            catch (WebException)
+            {
+                return false; 
+            }
+        }
+    }
+}
